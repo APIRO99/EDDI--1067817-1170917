6de23e7b507ee1c64151654c232b1447c863ff88
Andres Pineda <apiro99@outlook.com>
2018-02-13 17:09:51 -0600

    view

---
+++ Project_1067817_1170917/Project_1067817_1170917/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_1067817_1170917.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Last_Name { get; set; }
+        public int Salary { get; set; }
+        public string Club { get; set; }
+        public string Rol { get; set; }
+    }
+}
---
+++ Project_1067817_1170917/Project_1067817_1170917/Models/Project_1067817_1170917Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Project_1067817_1170917.Models
+{
+    public class Project_1067817_1170917Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Project_1067817_1170917Context() : base("name=Project_1067817_1170917Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Project_1067817_1170917.Models.Player> Players { get; set; }
+    }
+}
