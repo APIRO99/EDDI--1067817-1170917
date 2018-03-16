452b185aba010c0e3823a81228aabc84ee58fbb5
Andres Pineda <apiro99@outlook.com>
2018-03-15 18:53:26 -0600

    ??

---
+++ Lab03/Lab03/Lab03/Controllers/AVLTreeController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab03.Controllers
+{
+    public class AVLTreeController : Controller
+    {
+        // GET: AVLTree
+        public ActionResult Index()
+        {
+            try
+            {
+                if (Request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var fileName = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/App_Data/"), fileName);
+                        file.SaveAs(path);
+                        TempData["uploadResult"] = "Archivo subido con éxito";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                TempData["uploadResult"] = "Error" + ex.Message;
+
+            }
+            return View("Index");
+        }
+
+        // GET: AVLTree/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AVLTree/Upload
+        //   [HttpPost]
+        public ActionResult Upload()
+        {
+            try
+            {
+                if (Request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var fileName = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/App_Data/"), fileName);
+                        file.SaveAs(path);
+                        TempData["uploadResult"] = "Archivo subido con éxito";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                TempData["uploadResult"] = "Error" + ex.Message;
+
+            }
+            return View("Index");
+        }
+
+        // GET: AVLTree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AVLTree/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AVLTree/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AVLTree/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AVLTree/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AVLTree/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Lab03/Lab03/Lab03/Models/AVLTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab03.Models
+{
+    public class AVLTree<T>
+    {
+        private  class Node
+        {
+            T elemento;
+            AVLTree<T> Left;
+            AVLTree<T> Right;
+        }
+
+        AVLTree<T> root;
+
+    }
+
+    public class Match : IComparable
+    {
+            public int NoMatch { get; set; }
+            public DateTime MatchDay { get; set; }
+            public string Group { get; set; }
+            public string Team1 { get; set; }
+            public string Team2 { get; set; }
+            public string Stadium { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Match compareToObj = (Match)obj;
+            return this.NoMatch.CompareTo(compareToObj.NoMatch);
+        }
+
+    }
+}
